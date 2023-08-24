153daeb5109865fb99ec163682f34563bff35234
Charl913 <charlkruger61@gmail.com>
2023-08-24 15:38:55 +0200

    second commit

---
+++ Exercise/Exercise/QuotesPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Exercise
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class QuotesPage : ContentPage
+    {
+        public QuotesPage()
+        {
+            InitializeComponent();
+
+            Quote.Text = "Life is like riding a bicyle. To keep your balance, You must keep moving";
+        }
+
+        public int postion = 1;
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            
+            List<string> list = new List<string>();
+            list.Add("Life is like riding a bicyle. To keep your balance, You must keep moving");
+            list.Add("You can't blame gravity for falling in love");
+            list.Add("Look deep into nature, and then you will understand everyhing better");
+
+            if(postion < 3)
+            {
+                Quote.Text = list[postion];
+            }
+            else 
+            {
+                postion = 0;
+                Quote.Text = list[postion];
+            }
+
+            postion++;
+
+        }
+    }
+}
