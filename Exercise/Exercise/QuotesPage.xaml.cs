ae7fe066d1d5a00cb2e6d7868b5c84d6a9191a49
Charl913 <charlkruger61@gmail.com>
2023-08-28 17:38:47 +0200

    better practice update

--- Exercise/Exercise/QuotesPage.xaml.cs
+++ Exercise/Exercise/QuotesPage.xaml.cs
@@ -12,34 +12,35 @@ namespace Exercise
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class QuotesPage : ContentPage
     {
+        private string[] quoteList = new string[]
+        {
+            "Life is like riding a bicycle. To keep your balance, you must keep moving.",
+            "You can't blame gravity for falling in love.",
+            "Look deep into nature, and then you will understand everything better."
+        };
         public QuotesPage()
         {
             InitializeComponent();
 
-            Quote.Text = "Life is like riding a bicyle. To keep your balance, You must keep moving";
+            Quote.Text = quoteList[0];
+
+
         }
 
-        public int postion = 1;
+        public int position = 1;
+
         private void Button_Clicked(object sender, EventArgs e)
         {
-            
-            List<string> list = new List<string>();
-            list.Add("Life is like riding a bicyle. To keep your balance, You must keep moving");
-            list.Add("You can't blame gravity for falling in love");
-            list.Add("Look deep into nature, and then you will understand everyhing better");
-
-            if(postion < 3)
+            if (position < 3)
             {
-                Quote.Text = list[postion];
+                Quote.Text = quoteList[position];
             }
-            else 
+            else
             {
-                postion = 0;
-                Quote.Text = list[postion];
+                position = 0;
+                Quote.Text = quoteList[position];
             }
-
-            postion++;
-
+            position++;
         }
     }
 }
